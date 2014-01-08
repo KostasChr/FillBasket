c501c91693c150a180248a047e1c4d060ba8fe03
Kostas Christidis <costas.x@gmail.com>
2014-01-08 14:58:12 +0200

    Added Spring support

---
+++ src/main/java/gr/ntua/imu/fillbasket/recommender/Recommender.java
@@ -0,0 +1,12 @@
+package gr.ntua.imu.fillbasket.recommender;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kostas
+ * Date: 8/1/2014
+ * Time: 2:14 μμ
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Recommender {
+    void runAndEvaluate();
+}
